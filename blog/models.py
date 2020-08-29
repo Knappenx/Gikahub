5062a646c10e9753f897bdb9c5fc40933918e7e1
Rodrigo Lopez <rlopez0689@gmail.com>
2020-08-28 20:00:08 -0500

    Add modifications to model and first index page

--- blog/models.py
+++ blog/models.py
@@ -5,27 +5,30 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=20, blank=True, null=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Tags(models.Model):
     name = models.CharField(max_length=20, blank=True, null=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-        #on delete not really necessary as we want to keep posts
+    author = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
     created_at = models.DateTimeField(auto_now_add=True)
     published_date = models.DateTimeField(auto_now_add=False)
     post_title = models.CharField(max_length=50, blank=True, null=True)
-    post_category = models.ForeignKey(Category, on_delete=models.CASCADE)
-        # on delete not really necessary as we want to keep posts
+    post_category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
     post_tags = models.ManyToManyField(Tags)
-        #could show list of available ones (dropdown list with write in function)
     post_abstract = models.CharField(max_length=500, blank=True, null=True)
     post_slug = models.SlugField(max_length=60)
-        #Could have suggested slug with title
     post_main_image = models.URLField()
     post_is_draft = models.BooleanField(default=True)
-    post_view_counts = models.IntegerField
-        #Or use IntegerField
+    post_view_counts = models.IntegerField(default=0)
+    post_main_header = models.BooleanField(default=False)
 
-
+    def __str__(self):
+        return f'{self.post_title} - {self.post_category}'
