7c0e7e85158d4bb276d09e057e1523125a5e4b31
Knappenx <61886186+Knappenx@users.noreply.github.com>
2020-11-13 17:57:17 -0600

    Started post template and added other minor features

--- blog/views.py
+++ blog/views.py
@@ -9,10 +9,22 @@ def index(request):
     main_post = main_posts[0]
     secondary_posts = main_posts[1:3]
     third_posts = main_posts[3:5]
+    categories = {}
+    for post in posts:
+        if post.post_category not in categories:
+            categories[post.post_category] = [post]
+        else:
+            categories[post.post_category].append(post)
     post_context = {
         'main_posts': main_posts,
         'main_post': main_post,
         'secondary_posts': secondary_posts,
-        'third_posts': third_posts
+        'third_posts': third_posts,
+        'categories': categories
     }
     return render(request, 'blog/index.html', post_context)
+
+
+def template(request):
+    context = {}
+    return render(request, 'blog/post_template.html', context)
